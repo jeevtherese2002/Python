ee67902475d84e5f1f1120a41b5cc097db842502
jeevtherese2002 <jeevtherese@gmail.com>
2025-07-05 18:16:55 +0530

    Update C-3 exp2.py

--- C-3 exp2.py
+++ C-3 exp2.py
@@ -5,8 +5,8 @@ from graphics.graphics2.sphere import *
 
 import math
 
-print("Name : Niranjana S Nair")
-print("Admission No: A24MCA047")
+print("Name : Jeev therese v mathew ")
+print("Admission No: A24MCA034")
 print("Experiment No: 2")
 
 num=int(input("Enter the number to find square root"))
