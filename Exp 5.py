dfcddbd651b33e05a3383d5302f4bc0eafec8def
jeevtherese2002 <jeevtherese@gmail.com>
2025-07-05 18:21:55 +0530

    Update Exp 5.py

--- Exp 5.py
+++ Exp 5.py
@@ -1,5 +1,5 @@
-print("Niranjana S Nair")
-print("Admission no : A24MCA047 ")
+print("Name : Jeev therese v mathew ")
+print("Admission No: A24MCA034")
 print("Ascending & Descending Experiemnt : 5")
 fruits={'Mango': 3, 'Grapes' :2, 'Apple':4, 'Banana':1}
 
