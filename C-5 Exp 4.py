100aa6a9be9ce78d89b99c66f639c0a35534cd7a
jeevtherese2002 <jeevtherese@gmail.com>
2025-07-05 18:19:19 +0530

    Update C-5 Exp 4.py

--- C-5 Exp 4.py
+++ C-5 Exp 4.py
@@ -1,6 +1,6 @@
 import csv
-print("Name : Niranjana S Nair")
-print("Roll No: 47")
+print("Name : Jeev therese v mathew ")
+print("Admission No: A24MCA034")
 print("Experiment No: 18")
 
 columns_to_read = [0, 2]
