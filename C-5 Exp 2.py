d43a665b0627ca3497daa2737c7f8a909f63d07f
jeevtherese2002 <jeevtherese@gmail.com>
2025-07-05 18:18:55 +0530

    Update C-5 Exp 2.py

--- C-5 Exp 2.py
+++ C-5 Exp 2.py
@@ -1,5 +1,5 @@
-print("Name : Niranjana S Nair")
-print("Roll No: 47")
+print("Name : Jeev therese v mathew ")
+print("Admission No: A24MCA034")
 print("Experiment No: 16")
 
 with open("file.txt", "r") as infile, open("output.txt", "w") as outfile:
